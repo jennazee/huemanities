298ec31fcd3a5eced61cca73f939141c2150aec3
Jenna Zeigen <jenna.zeigen@gmail.com>
2017-05-15 00:16:19 -0400

    initial commit

---
+++ Artifier.js
@@ -0,0 +1,43 @@
+export default class Artifier {
+  constructor() {
+    this.artifyImageOnload();
+  }
+
+  artifyImageWithOnload() {
+    document.addEventListener('DOMContentLoaded', function() {
+      const canvas = document.querySelector('.Canvas');
+      const canvasCtx = canvas.getContext('2d');
+
+      const uploader = document.querySelector('.Uploader');
+
+      uploader.addEventListener('change', function(){
+        let img = new Image();
+        let reader = new FileReader();
+        reader.onload = function(e) {
+          img.src = e.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+        img.onload = function() {
+          canvas.height = img.height;
+          canvas.width = img.width;
+          canvasCtx.drawImage(img, 0, 0);
+
+          let newImgData = new ImageData(this.artFunction(canvasCtx, canvas.width, canvas.height), img.width);
+
+          canvasCtx.putImageData(newImgData, 0, 0);
+        };
+      });
+    });
+  };
+
+  artFunction() { }
+
+  getPixelAt(h, w, imWidth, imageData) {
+    let redIndex = 4 * h * imWidth + w * 4;
+    let red = imageData[redIndex];
+    let green = imageData[redIndex + 1];
+    let blue = imageData[redIndex + 2];
+    let alpha = imageData[redIndex + 3];
+    return [red, green, blue, alpha];
+  }
+}
---
+++ glitchrothko.js
@@ -0,0 +1,72 @@
+import Artifier from './Artifier.js';
+
+export default class GlitchRothko extends Artifier {
+  constructor() {
+    super();
+  }
+
+  artFunction(ctx, width, height) {
+    let imageData = ctx.getImageData(0, 0, width, height).data;
+    let reds = [];
+    let greens = [];
+    let blues = [];
+    let grayscales = [];
+
+    for (let h = 0; h < height; h++) {
+      for (let w = 0; w < width; w++) {
+        let current = getPixelAt(h, w, width, imageData);
+
+        let winnerType = this.getHighestOfThree(current);
+
+        if (winnerType === 'red') {
+          this.appendArray(reds, current);
+        }
+        if (winnerType === 'green') {
+          this.appendArray(greens, current);
+        }
+        if (winnerType === 'blue') {
+          this.appendArray(blues, current);
+        }
+        if (winnerType === 'grayscale') {
+          this.appendArray(grayscales, current);
+        }
+      }
+    }
+
+    let newImageArray = [];
+    this.appendArray(newImageArray, reds);
+    this.appendArray(newImageArray, greens);
+    this.appendArray(newImageArray, blues);
+    this.appendArray(newImageArray, grayscales);
+    return new Uint8ClampedArray(newImageArray);
+  }
+
+  getHighestOfThree(arr) {
+    let winnerType, winnerIndex;
+
+    if (arr[0] === arr[1] && arr[0] === arr[2]) {
+      return 'grayscale';
+    }
+
+    if (arr[0] > arr[1]) {
+      winnerType = 'red';
+      winnerIndex = 0;
+    } else {
+      winnerType = 'green';
+      winnerIndex = 1;
+    }
+
+    if (arr[2] > arr[winnerIndex]) {
+      winnerType = 'blue';
+    }
+
+    return winnerType;
+  }
+
+  //TODO: use spread operator maybe?
+  appendArray(arr1, arr2) {
+    for (let i = 0; i < arr2.length; i++) {
+      arr1.push(arr2[i]);
+    }
+  }
+}
